e3253325ded4519936c36c0e31d47b4e6ae6d258
Adam Gibbs <adam-gibbs@outlook.com>
2022-02-14 02:01:12 +0000

    Add shake

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,29 @@
 <template>
   <div style="font-size: 0">
     <div
-      class="rounded-2xl mx-auto my-2 overflow-hidden grid grid-cols-32 w-full max-w-lg gap-0"
+      class="transform rounded-2xl mx-auto my-2 overflow-hidden grid grid-cols-32 w-full sm:max-w-lg max-w-xs gap-0"
+      :class="disabled ? (shake ? '-rotate-1' : 'rotate-1') : ''"
     >
       <div
         v-for="n in squareNumber ** 2"
         :key="n"
         :ref="n.toString()"
-        class="h-4 border-none m-0 p-0 bg-black inline-block"
+        class="sm:h-4 h-2.5 border-none m-0 p-0 bg-black inline-block"
         :style="`opacity: 0.1;`"
       />
     </div>
   </div>
-  <button class="mx-auto px-10 py-2 flex border-2 rounded-lg hover:bg-gray-200">
+  <button
+    class="mx-auto px-10 py-2 flex border-2 rounded-lg hover:bg-gray-200"
+    :disabled="disabled"
+    @click="reset"
+  >
     New
   </button>
   <div class="flex pt-1">
     <label for="x-axis">X Axis</label>
     <input
+      :disabled="disabled"
       type="range"
       class="w-2/6 flex mx-auto bg-red-500 focus:outline-none focus:ring-0 focus:shadow-none"
       :max="squareNumber"
@@ -27,6 +33,7 @@
     />
     <label for="y-axis">Y Axis</label>
     <input
+      :disabled="disabled"
       type="range"
       class="w-2/6 flex mx-auto bg-blue-500 focus:outline-none focus:ring-0 focus:shadow-none"
       :max="squareNumber - 1"
@@ -44,10 +51,10 @@ export default defineComponent({
   data() {
     return {
       squareNumber: 32,
-      containerWidth: this.containerSize(),
-      squareWidth: this.containerSize() / 32,
       currentSquare: 0,
+      disabled: false,
       interval: null,
+      shake: false,
       xaxis: 1,
       yaxis: 0,
     };
@@ -67,30 +74,80 @@ export default defineComponent({
       }, 500);
     },
     xaxis(previous, target) {
-      clearInterval(this.interval);
-      if (this.checkJump(previous, target)) {
-        this.drawTo(previous, target, "x");
+      if (!this.disabled) {
+        clearInterval(this.interval);
+        if (this.checkJump(previous, target)) {
+          this.drawTo(previous, target, "x");
+        }
+        this.currentSquare = this.calculateSquare(this.xaxis, this.yaxis);
       }
-      this.currentSquare = this.calculateSquare(this.xaxis, this.yaxis);
     },
     yaxis(previous, target) {
-      clearInterval(this.interval);
-      if (this.checkJump(previous, target)) {
-        this.drawTo(previous, target, "y");
+      if (!this.disabled) {
+        clearInterval(this.interval);
+        if (this.checkJump(previous, target)) {
+          this.drawTo(previous, target, "y");
+        }
+        this.currentSquare = this.calculateSquare(this.xaxis, this.yaxis);
       }
-      this.currentSquare = this.calculateSquare(this.xaxis, this.yaxis);
     },
   },
 
   methods: {
-    containerSize() {
-      return window.innerWidth < window.innerHeight - 95
-        ? window.innerWidth - 20
-        : window.innerHeight - 95;
+    reset() {
+      this.xaxis = 1;
+      this.yaxis = 0;
+      this.currentSquare = 0;
+      clearInterval(this.interval);
+      this.disabled = true;
+      const self = this;
+      let loop = 0;
+      const clearLoop = setInterval(function () {
+        for (let square = 1; square <= self.squareNumber ** 2; square++) {
+          self.colourReduce(square);
+        }
+        self.shake = !self.shake;
+        loop++;
+
+        if (loop === 10) {
+          for (let i = 1; i <= self.squareNumber ** 2; i++) {
+            self.clearSquare(i);
+          }
+          self.disabled = false;
+          clearInterval(clearLoop);
+        }
+      }, 30);
+
+      this.interval = setInterval(function () {
+        self.colourIn(self.currentSquare);
+      }, 500);
+    },
+
+    clearSquare(square) {
+      if (this.checkSquare(square)) {
+        try {
+          this.$refs[square.toString()][0].style.opacity = 0.1;
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    },
+
+    colourReduce(square) {
+      if (this.checkSquare(square)) {
+        try {
+          let currentOpacity = +this.$refs[square.toString()][0].style.opacity;
+          if (currentOpacity > 0.1) {
+            this.$refs[square.toString()][0].style.opacity -= 0.1;
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
     },
 
     colourIn(square) {
-      if (square <= this.squareNumber ** 2 && square > 0) {
+      if (this.checkSquare(square)) {
         try {
           let currentOpacity = +this.$refs[square.toString()][0].style.opacity;
           if (currentOpacity < 1) currentOpacity += 0.1;
@@ -99,6 +156,10 @@ export default defineComponent({
           console.log(e);
         }
       }
+    },
+
+    checkSquare(square) {
+      return square <= this.squareNumber ** 2 && square > 0;
     },
 
     checkJump(previous, target) {
